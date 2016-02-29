380cb1dd2a0b53dea348dac20e6cb1a6626608de
ebrunetta <esteban.brunetta@gmail.com>
2016-02-29 20:03:57 -0300

    issues #5 : Se agrega licencia en clases

--- dom/src/main/java/dom/historiaclinica/HistoriaClinicaServicio.java
+++ dom/src/main/java/dom/historiaclinica/HistoriaClinicaServicio.java
@@ -1,3 +1,18 @@
+/*
+ Copyright 2015 Adamantium
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package dom.historiaclinica;
 
 import java.io.File;
@@ -449,11 +464,9 @@ public class HistoriaClinicaServicio extends AbstractFactoryAndRepository {
 			ReporteReceta receta = new ReporteReceta();
 			receta.setPaciente(a.getPaciente().getApellido() + " "
 					+ a.getPaciente().getNombre());
-			if (a.getObraSocial() != null) {
+			
 				receta.setObraSocial(a.getObraSocial().getNombre());
-			} else {
-				receta.setObraSocial(" ");
-			}
+			
 
 			receta.setMedicamento(a.getMedicamento().getProducto() + " "
 					+ a.getMedicamento().getPresentacion() + " "
@@ -571,18 +584,11 @@ public class HistoriaClinicaServicio extends AbstractFactoryAndRepository {
 		general.setPaciente(a.getPaciente().getApellido() + " "
 				+ a.getPaciente().getNombre());
 		general.setEstadoCivil(a.getEstadoCivil().toString());
-		if (a.getObraSocial()) {
-			general.setObraSocial(a.getPaciente().getObraSocial().getNombre());
-		} else {
-			general.setObraSocial("No tiene obra Social");
-		}
-
+		general.setObraSocial(a.getPaciente().getObraSocial().getNombre());
 		general.setEducacion(a.getEducacion().getNombre());
-
 		general.setTrabajo(pasarASiONo(a.getTrabajo()));
 		general.setDni(a.getPaciente().getDocumento());
 		SimpleDateFormat df = new SimpleDateFormat("dd-MM-YYYY");
-
 		general.setFechanac(df.format(a.getPaciente().getFechaNacimiento()
 				.toDate()));
 		general.setNumCarnet(a.getPaciente().getNumerodeCarnet());
